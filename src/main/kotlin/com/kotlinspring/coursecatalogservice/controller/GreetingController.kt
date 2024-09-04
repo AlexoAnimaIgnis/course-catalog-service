5546dbd55d94b29abf180179ae180c205e82ec71
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-09-04 19:10:34 +0800

    kotlin-spring additional setup

--- src/main/kotlin/com/kotlinspring/coursecatalogservice/controller/GreetingController.kt
+++ src/main/kotlin/com/kotlinspring/coursecatalogservice/controller/GreetingController.kt
@@ -1,6 +1,7 @@
 package com.kotlinspring.coursecatalogservice.controller
 
 import com.kotlinspring.coursecatalogservice.service.GreetingService
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,8 +13,11 @@ class GreetingController(
     val greetingService: GreetingService
 ) {
 
+    companion object: KLogging()
+
     @GetMapping("/{name}")
     fun retrieveGreetings(@PathVariable("name") name: String) : String {
+        logger.info("Name is $name")
         return greetingService.retrieveGreetings(name)
     }
 }
