e74e73faff470972af249ee33b2f44261d208c6f
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-09-11 16:19:14 +0800

    implemented put method

--- src/main/kotlin/com/kotlinspring/service/CourseService.kt
+++ src/main/kotlin/com/kotlinspring/service/CourseService.kt
@@ -2,6 +2,7 @@ package com.kotlinspring.service
 
 import com.kotlinspring.dto.CourseDto
 import com.kotlinspring.entity.Course
+import com.kotlinspring.exception.CourseNotFoundException
 import com.kotlinspring.repository.CourseRepository
 import mu.KLogging
 import org.springframework.stereotype.Service
@@ -27,6 +28,22 @@ class CourseService(val courseRepository: CourseRepository) {
     fun retrieveAllCourses(): List<CourseDto> {
         return courseRepository.findAll().map {
             CourseDto(it.id, it.name, it.category)
+        }
+    }
+
+    fun updateCourse(courseId: Int, courseDto: CourseDto): CourseDto {
+        val existingCourse = courseRepository.findById(courseId)
+
+        return if(existingCourse.isPresent) {
+            existingCourse.get()
+                .let {
+                    it.name = courseDto.name
+                    it.category = courseDto.category
+                    courseRepository.save(it)
+                    CourseDto(it.id, it.name, it.category)
+                }
+        } else {
+            throw CourseNotFoundException("No course found for id: $courseId")
         }
     }
 
