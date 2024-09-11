debd0b58b98690881677e0dbcf4547cec3a81c3e
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-09-11 16:07:43 +0800

    added intg test for findAllCourses

--- src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
+++ src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
@@ -1,12 +1,16 @@
 package com.kotlinspring.controller
 
 import com.kotlinspring.dto.CourseDto
+import com.kotlinspring.repository.CourseRepository
+import com.kotlinspring.util.courseEntityList
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,6 +20,16 @@ class CourseControllerIntgTest {
 
     @Autowired
     lateinit var webTestClient: WebTestClient
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setup() {
+        courseRepository.deleteAll()
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
 
     @Test
     fun addCourse() {
@@ -31,5 +45,19 @@ class CourseControllerIntgTest {
             .responseBody
 
         assertNotNull(savedCourseDto!!.id)
+    }
+
+    @Test
+    fun retrieveAllCourses() {
+        val courseDtos = webTestClient
+            .get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDto::class.java)
+            .returnResult()
+            .responseBody
+
+        assertEquals(3, courseDtos!!.size)
     }
 }
