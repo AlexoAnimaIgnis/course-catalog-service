4e886a74b87e5e1681c606e6c0ece14e9583db2e
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-09-12 19:44:49 +0800

    test case for get endpoint

--- src/test/unit/com/kotlinspring/controller/CourseControllerUnitTest.kt
+++ src/test/unit/com/kotlinspring/controller/CourseControllerUnitTest.kt
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.reactive.server.WebTestClient
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 @WebMvcTest(controllers = [CourseController::class]) // takes care of spinning up the controller
@@ -38,5 +39,30 @@ class CourseControllerUnitTest {
             .responseBody
 
         assertNotNull(savedCourseDto!!.id)
+    }
+
+    @Test
+    fun retrieveAllCourses() {
+
+        every { courseServiceMock.retrieveAllCourses() }.returnsMany(
+            listOf(
+                courseDTO(id = 1),
+                courseDTO(
+                    id = 2,
+                    name = "Build Reactive Microservices using Spring WebFlux/SpringBoot"
+                )
+            )
+        )
+
+        val courseDtos = webTestClient
+            .get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDto::class.java)
+            .returnResult()
+            .responseBody
+
+        assertEquals(2, courseDtos!!.size)
     }
 }
