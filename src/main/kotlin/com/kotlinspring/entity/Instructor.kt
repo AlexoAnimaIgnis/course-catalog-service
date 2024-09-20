e902a9ab520f251c437d60acac695a4341b2ace4
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-09-20 16:59:40 +0800

    establish relation between course and instructor

--- src/main/kotlin/com/kotlinspring/entity/Instructor.kt
+++ src/main/kotlin/com/kotlinspring/entity/Instructor.kt
@@ -1,16 +1,25 @@
 package com.kotlinspring.entity
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 
 @Entity
-@Table(name = "Instructors")
+@Table(name = "INSTRUCTORS")
 data class Instructor(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Int?,
-    val name: String
+    val name: String,
+    // to connect to courseEntity
+    @OneToMany(
+        mappedBy = "instructor",
+        cascade = [CascadeType.ALL],
+        orphanRemoval = true
+    )
+    var courses : List<Course> = mutableListOf()
 )
